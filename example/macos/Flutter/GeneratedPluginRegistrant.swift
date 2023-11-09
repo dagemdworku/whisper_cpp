79d7cdc6c259a4cf835524fa8c133819f6b94c0e
dagemdworku-dev <dagemdworku+dev@gmail.com>
2023-11-10 01:37:37 +0300

    initial configuration

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import whisper_cpp
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  WhisperCppPlugin.register(with: registry.registrar(forPlugin: "WhisperCppPlugin"))
+}
