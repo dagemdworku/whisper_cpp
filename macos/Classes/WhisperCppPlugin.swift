3d7463088148224e86091e472b04d8dad089c39c
dagemdworku-dev <dagemdworku+dev@gmail.com>
2023-11-12 05:30:37 +0300

    Add result event channel for WhisperCppPlugin

--- macos/Classes/WhisperCppPlugin.swift
+++ macos/Classes/WhisperCppPlugin.swift
@@ -4,6 +4,7 @@ import FlutterMacOS
 let kFLTWhisperCppMethodChannelName = "plugins.dagemdworku.io/whisper_cpp"
 let kFLTWhisperCppIsRecordingEvent = "whisper_cpp_is_recording_event"
 let kFLTWhisperCppStatusLogEvent = "whisper_cpp_status_log_event"
+let kFLTWhisperCppResultEvent = "whisper_cpp_result_event"
 
 public class WhisperCppPlugin: NSObject, FlutterPlugin {
     private var whisperState: WhisperState?
@@ -51,6 +52,7 @@ public class WhisperCppPlugin: NSObject, FlutterPlugin {
                 
                 registerIsRecordingEventChannel(whisperState: whisperState!)
                 registerStatusLogEventChannel(whisperState: whisperState!)
+                registerResultEventChannel(whisperState: whisperState!)
                 
                 let whisperConfigDict: [String: Any] = [
                     "modelConfig": whisperState!.whisperConfig!.modelConfig.toDictionary(),
@@ -91,6 +93,12 @@ public class WhisperCppPlugin: NSObject, FlutterPlugin {
         let eventChannelName = kFLTWhisperCppMethodChannelName + "/token/" + kFLTWhisperCppStatusLogEvent
         let eventChannel = FlutterEventChannel(name: eventChannelName, binaryMessenger: messenger)
         eventChannel.setStreamHandler(StatusLogStreamHandler(whisperState: whisperState))
+    }
+
+    private func registerResultEventChannel(whisperState: WhisperState) {
+        let eventChannelName = kFLTWhisperCppMethodChannelName + "/token/" + kFLTWhisperCppResultEvent
+        let eventChannel = FlutterEventChannel(name: eventChannelName, binaryMessenger: messenger)
+        eventChannel.setStreamHandler(ResultStreamHandler(whisperState: whisperState))
     }
     
     private func flutterError(from error: Error) -> FlutterError {
