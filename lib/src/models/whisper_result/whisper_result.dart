56c7220e997a227660b7d021e8a4c4c8468f9a61
dagemdworku-dev <dagemdworku+dev@gmail.com>
2023-11-13 00:37:52 +0300

    Add toLog method to WhisperResult class

--- lib/src/models/whisper_result/whisper_result.dart
+++ lib/src/models/whisper_result/whisper_result.dart
@@ -1,5 +1,7 @@
 import 'package:whisper_cpp/whisper_cpp.dart';
 
+const int _kConsoleWidth = 79;
+
 class WhisperResult {
   final int time;
   final String text;
@@ -27,5 +29,30 @@ class WhisperResult {
       'text': text,
       'tokenData': tokenData.toJson(),
     };
+  }
+
+  String toLog() {
+    int columnWidth = (_kConsoleWidth ~/ 2) - 3;
+
+    var logConfig = [
+      ['id', tokenData.id.toString()],
+      ['text', text],
+      ['probability', tokenData.p.toString()],
+      ['time offset', ConversionHelper.getMS(time).toString()],
+      ['token length', tokenData.vlen.toString()],
+      ['pt', tokenData.pt.toString()],
+      ['pt sum', tokenData.ptsum.toString()],
+    ].map((item) {
+      return '| ${item[0].padRight(columnWidth)} | ${(item[1]).padRight(columnWidth)} |';
+    }).join('\n');
+
+    String logMessage = '\n';
+    logMessage += '+${''.padRight(columnWidth + 2, '-')}+'
+        '${''.padRight(columnWidth + 2, '-')}+\n';
+    logMessage += logConfig;
+    logMessage += '\n+${''.padRight(columnWidth + 2, '-')}+'
+        '${''.padRight(columnWidth + 2, '-')}+';
+
+    return logMessage;
   }
 }
