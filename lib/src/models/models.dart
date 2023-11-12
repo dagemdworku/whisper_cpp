b24fd64cc75587056645093068ab9e1f14a709d8
dagemdworku-dev <dagemdworku+dev@gmail.com>
2023-11-12 05:31:05 +0300

    Add WhisperResult and WhisperTokenData models to models.dart

--- lib/src/models/models.dart
+++ lib/src/models/models.dart
@@ -1,3 +1,6 @@
 export 'whisper_config/whisper_config.dart';
 export 'whisper_config/whisper_compute_config.dart';
 export 'whisper_config/whisper_model_config.dart';
+
+export 'whisper_result/whisper_result.dart';
+export 'whisper_result/whisper_token_data.dart';
