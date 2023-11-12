2dc5ef1ae7e8f87ba2865d0c04c007e0749256fe
dagemdworku-dev <dagemdworku+dev@gmail.com>
2023-11-13 02:43:06 +0300

    Refactor log formatting in WhisperConfig and WhisperResult classes

--- lib/src/models/whisper_result/whisper_result.dart
+++ lib/src/models/whisper_result/whisper_result.dart
@@ -1,7 +1,5 @@
 import 'package:whisper_cpp/whisper_cpp.dart';
 
-const int _kConsoleWidth = 79;
-
 class WhisperResult {
   final int time;
   final int tokenBeg;
@@ -26,6 +24,20 @@ class WhisperResult {
     );
   }
 
+  WhisperResult copyWith({
+    int? time,
+    int? tokenBeg,
+    String? text,
+    WhisperTokenData? tokenData,
+  }) {
+    return WhisperResult(
+      time: time ?? this.time,
+      tokenBeg: tokenBeg ?? this.tokenBeg,
+      text: text ?? this.text,
+      tokenData: tokenData ?? this.tokenData,
+    );
+  }
+
   Map<String, dynamic> toJson() {
     return {
       'time': time,
@@ -36,28 +48,23 @@ class WhisperResult {
   }
 
   String toLog() {
-    int columnWidth = (_kConsoleWidth ~/ 2) - 3;
+    return LogHelper.parseTableContent({
+      'id': tokenData.id.toString(),
+      'tokenBeg': tokenBeg.toString(),
+      'text': text,
+      'probability': tokenData.p.toString(),
+      'time offset': '${ConversionHelper.getMS(time)} ms',
+      'token length': tokenData.vlen.toString(),
+      'pt': tokenData.pt.toString(),
+      'pt sum': tokenData.ptsum.toString(),
+    });
+  }
 
-    var logConfig = [
-      ['id', tokenData.id.toString()],
-      ['tokenBeg', tokenBeg.toString()],
-      ['text', text],
-      ['probability', tokenData.p.toString()],
-      ['time offset', '${ConversionHelper.getMS(time)} ms'],
-      ['token length', tokenData.vlen.toString()],
-      ['pt', tokenData.pt.toString()],
-      ['pt sum', tokenData.ptsum.toString()],
-    ].map((item) {
-      return '| ${item[0].padRight(columnWidth)} | ${(item[1]).padRight(columnWidth)} |';
-    }).join('\n');
-
-    String logMessage = '\n';
-    logMessage += '+${''.padRight(columnWidth + 2, '-')}+'
-        '${''.padRight(columnWidth + 2, '-')}+\n';
-    logMessage += logConfig;
-    logMessage += '\n+${''.padRight(columnWidth + 2, '-')}+'
-        '${''.padRight(columnWidth + 2, '-')}+';
-
-    return logMessage;
+  static String toLineLog(List<WhisperResult> results) {
+    return LogHelper.parseTableContent({
+      'start timestamp': TimestampHelper.fromInt(results.first.time),
+      'end timestamp': TimestampHelper.fromInt(results.last.time),
+      'text': results.map((e) => e.text).join(),
+    });
   }
 }
