09cf5c47775b29bd01bfd2cade3ec54948e8eebd
dagemdworku-dev <dagemdworku+dev@gmail.com>
2023-11-13 05:18:34 +0300

    UI redesign

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: avoid_print
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
@@ -19,12 +20,12 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final _whisperCppPlugin = WhisperCpp();
+  final List<List<WhisperResult>> _transcription = [];
 
   late StreamSubscription<bool> _isRecordingStreamSubscription;
   late StreamSubscription<bool> _isModelLoadedStreamSubscription;
   late StreamSubscription<bool> _canTranscribeStreamSubscription;
   late StreamSubscription<dynamic> _statusLogStreamSubscription;
-  late StreamSubscription<WhisperResult?> _resultStreamSubscription;
   late StreamSubscription<List<WhisperResult>> _resultsStreamSubscription;
   late StreamSubscription<WhisperSummary?> _summaryStreamSubscription;
 
@@ -33,7 +34,6 @@ class _MyAppState extends State<MyApp> {
   bool _isModelLoaded = false;
   bool _canTranscribe = false;
   String _statusLog = '';
-  String _result = '';
 
   @override
   void initState() {
@@ -52,9 +52,8 @@ class _MyAppState extends State<MyApp> {
 
     if (!mounted) return;
 
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+    _platformVersion = platformVersion;
+    setState(() {});
   }
 
   @override
@@ -62,38 +61,120 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: Text('Whisper Cpp Plugin $_platformVersion'),
         ),
         body: Center(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Text('Running on: $_platformVersion\n'),
-              Text('Is Recording: $_isRecording\n'),
-              Text('Is Model Loaded: $_isModelLoaded\n'),
-              Text('Can Transcribe: $_canTranscribe\n'),
-              Text('Status: $_statusLog\n'),
-              ElevatedButton(
-                onPressed: _initialize,
-                child: const Text('Initialize'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    TextButton(
+                      onPressed: _initialize,
+                      child: const Text('Initialize'),
+                    ),
+                    ElevatedButton(
+                      onPressed: _toggleRecord,
+                      child: Text(
+                        _isRecording ? 'Stop Recording' : 'Start Recording',
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: _canTranscribe ? _transcribe : null,
+                      child: const Text('Transcribe'),
+                    ),
+                    TextButton(
+                      onPressed: _clear,
+                      child: const Text('Clear'),
+                    ),
+                  ],
+                ),
               ),
-              ElevatedButton(
-                onPressed: () async {
-                  await _whisperCppPlugin.toggleRecord();
-                },
-                child: Text(_isRecording ? 'Stop' : 'Start'),
+              const Divider(height: 0),
+              Expanded(
+                child: ListView.separated(
+                  padding: const EdgeInsets.all(8.0),
+                  itemBuilder: (BuildContext context, int index) =>
+                      _buildTranscription(_transcription[index]),
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const SizedBox(height: 8.0),
+                  itemCount: _transcription.length,
+                ),
               ),
-              ElevatedButton(
-                onPressed: _canTranscribe ? () async {
-                  await _whisperCppPlugin.transcribe();
-                } : null,
-                child: const Text('Transcribe'),
+              const Divider(height: 0),
+              SizedBox(
+                height: 100.0,
+                child: SingleChildScrollView(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    _statusLog,
+                    style: TextStyle(color: Colors.grey.shade600, fontSize: 12.0),
+                  ),
+                ),
               ),
-              const Divider(),
-              Text('Result: $_result\n'),
+              const Divider(height: 0),
+              Padding(
+                padding: const EdgeInsets.all(6.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    _buildStatus('isModelLoaded', _isModelLoaded),
+                    _buildStatus('isRecording', _isRecording),
+                    _buildStatus('canTranscribe', _canTranscribe),
+                  ],
+                ),
+              )
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildStatus(String title, bool value) {
+    return Material(
+      color: (value ? Colors.green : Colors.red).shade100,
+      borderRadius: BorderRadius.circular(4.0),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              value
+                  ? CupertinoIcons.checkmark_alt_circle
+                  : CupertinoIcons.clear_circled,
+              size: 16.0,
+              color: value ? Colors.green : Colors.red,
+            ),
+            Text(
+              ' $title',
+              style: const TextStyle(height: 1.1),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTranscription(List<WhisperResult> transcription) {
+    String text = transcription.map((e) => e.text).join();
+    String startTimestamp = TimestampHelper.fromInt(transcription.first.time);
+    String endTimestamp = TimestampHelper.fromInt(transcription.last.time);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(text.trim()),
+        const SizedBox(height: 4.0),
+        Text(
+          '$startTimestamp - $endTimestamp',
+          style: const TextStyle(fontSize: 12.0, color: Colors.grey),
+        ),
+      ],
     );
   }
 
@@ -109,7 +190,6 @@ class _MyAppState extends State<MyApp> {
       _registerIsModelLoadedChangeListener();
       _registerCanTranscribeChangeListener();
       _registerStatusLogChangeListener();
-      // _registerResultChangeListener();
       _registerResultsChangeListener();
       _registerSummaryChangeListener();
     } on WhisperCppException catch (e) {
@@ -118,6 +198,19 @@ class _MyAppState extends State<MyApp> {
       print('WhisperCppException tips: ${e.tips}');
       print(e.toString());
     }
+  }
+
+  Future<void> _toggleRecord() async {
+    await _whisperCppPlugin.toggleRecord();
+  }
+
+  Future<void> _transcribe() async {
+    await _whisperCppPlugin.transcribe();
+  }
+
+  void _clear() {
+    _transcription.clear();
+    setState(() {});
   }
 
   void _registerIsRecordingChangeListener() {
@@ -146,34 +239,20 @@ class _MyAppState extends State<MyApp> {
 
   void _registerStatusLogChangeListener() {
     _statusLogStreamSubscription = WhisperCpp.statusLog.listen((String event) {
-      _statusLog = event;
+      _statusLog += '$event\n';
       setState(() {});
     });
   }
-
-  // void _registerResultChangeListener() {
-  //   _resultStreamSubscription = WhisperCpp.result.listen((
-  //     WhisperResult? event,
-  //   ) {
-  //     if (event == null) return;
-
-  //     _result += event.text;
-  //     if (event.tokenData.id > event.tokenBeg) _result += '\n';
-
-  //     print('result: ${event.toLog()}');
-
-  //     setState(() {});
-  //   });
-  // }
 
   void _registerResultsChangeListener() {
     _resultsStreamSubscription = WhisperCpp.results.listen((
       List<WhisperResult> event,
     ) {
-      _result += event.map((e) => e.text).join();
+      _transcription.add(event);
+      // _result += event.map((e) => e.text).join();
 
       print('results: ${WhisperResult.toLineLog(event)}');
-      
+
       setState(() {});
     });
   }
@@ -193,7 +272,6 @@ class _MyAppState extends State<MyApp> {
     _isModelLoadedStreamSubscription.cancel();
     _canTranscribeStreamSubscription.cancel();
     _statusLogStreamSubscription.cancel();
-    _resultStreamSubscription.cancel();
     _resultsStreamSubscription.cancel();
     _summaryStreamSubscription.cancel();
     super.dispose();
