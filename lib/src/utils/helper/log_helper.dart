cf16ed9a5fed7eb90ed7d7fd56bb8964fb06068e
dagemdworku-dev <dagemdworku+dev@gmail.com>
2023-11-13 03:04:56 +0300

    Refactor table content parsing in LogHelper class.

--- lib/src/utils/helper/log_helper.dart
+++ lib/src/utils/helper/log_helper.dart
@@ -4,8 +4,10 @@ abstract class LogHelper {
   static String parseTableContent(Map<String, String> map) {
     int columnWidth = (_kConsoleWidth ~/ 2) - 3;
 
-    String body = map.entries.map((entry) {
-      return '| ${entry.key.padRight(columnWidth)} | ${(entry.value).padRight(columnWidth)} |';
+    String body = _spiteContent(map, columnWidth).map((entry) {
+      if (entry.length != 2) return '';
+
+      return '| ${entry[0].padRight(columnWidth)} | ${(entry[1]).padRight(columnWidth)} |';
     }).join('\n');
 
     String content = '\n';
@@ -14,6 +16,30 @@ abstract class LogHelper {
     content += body;
     content += '\n+${''.padRight(columnWidth + 2, '-')}+'
         '${''.padRight(columnWidth + 2, '-')}+';
+
+    return content;
+  }
+
+  static List<List<String>> _spiteContent(
+    Map<String, String> map,
+    int columnWidth,
+  ) {
+    List<List<String>> content = [];
+
+    map.forEach((key, value) {
+      List<String> values = RegExp('.{1,$columnWidth}')
+          .allMatches(value.trim())
+          .map((m) => m.group(0) ?? '')
+          .toList();
+
+      for (int i = 0; i < values.length; i++) {
+        if (i == 0) {
+          content.add([key.trim(), values[i]]);
+        } else {
+          content.add(['', values[i]]);
+        }
+      }
+    });
 
     return content;
   }
